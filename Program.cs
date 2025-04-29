282708958abe84b33ccdbf1cbe429ad14068f555
tryg thomas <thomastryg@gmail.com>
2025-04-29 10:42:39 +0200

    First git test.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string input = Console.ReadLine();
+        
+        int wordCount = WordCounter.CountWords(input);
+        Console.WriteLine($"Number of words in the sentence: {wordCount}");
+    }
+}
+
+public class WordCounter
+{
+    public static int CountWords(string sentence)
+    {
+        if (string.IsNullOrWhiteSpace(sentence))
+            return 0;
+
+        string[] words = sentence.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
